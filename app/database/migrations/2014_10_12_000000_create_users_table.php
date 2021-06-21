8e583d63c243a7d9c4a849000fe34ea958d521ea
naoto-naka <luckywhitecat770@gmail.com>
2021-06-22 07:20:58 +0900

    :sparkles:Update users migration
    ユーザ用のカラムをデフォルトの内容から変更

--- app/database/migrations/2014_10_12_000000_create_users_table.php
+++ app/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,17 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('last_name');
+            $table->string('first_name');
+            $table->tinyInteger('age')->unsigned();
+            $table->tinyInteger('gender')->unsigned();
+            $table->tinyInteger('score')->unsigned();
+            $table->tinyInteger('time')->unsigned();
+            $table->tinyInteger('status')->unsigned();
+            $table->string('token');
+            $table->tinyInteger('token_used_flag')->unsigned();
+            $table->dateTime('test_datetime');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
