e7c3399dae7bba091e3c0fa614c57ebdd1511bac
naoto-naka <luckywhitecat770@gmail.com>
2021-07-06 07:22:57 +0900

    :sparkles:Add Vue components
    問題作成ページのフォーム用のコンポーネントを作成

--- app/resources/js/app.js
+++ app/resources/js/app.js
@@ -1,1 +1,30 @@
 require('./bootstrap');
+
+import Vue from 'vue'
+import axios from 'axios'
+
+import QuestionForm from './components/QuestionForm.vue'
+
+new Vue({
+  el: '#app',
+  components: {
+    QuestionForm
+  },
+  data() {
+    return {
+      showForm: false,
+      choice: false
+    }
+  },
+  methods: {
+    selectFormType(formType, e) {
+      this.showForm = true;
+      e.target.classList.add("active");
+      console.log(e.target)
+
+      if (formType === "radio" || formType === "select") {
+        this.choice = true;
+      }
+    }
+  }
+});
--- app/resources/views/admin/layouts/base.blade.php
+++ app/resources/views/admin/layouts/base.blade.php
@@ -4,7 +4,9 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  <link rel="stylesheet" href="css/style.css">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+  <script src="{{ mix('js/app.js') }}" defer></script>
   <title>@yield('title')</title>
 </head>
 <body>
